a733662290435a3cf1f3df268f4add715e0e5715
Felix Schmidt <s224313@dtu.dk>
2024-02-22 19:56:16 +0100

    first prototype

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     discountQuantity: 2,
     discountPercent: 10,
     upsellProductId: null
-},
+    },
     {
     id: "banana",
     name: "Banana bag",
@@ -41,12 +41,68 @@ function App() {
     discountQuantity: 3,
     discountPercent: 15,
     upsellProductId: null
+    }];
+    const [basket, setBasket] =
+        useState(Array(products.length).fill(null));
+    function handleAmountChange(index: number, amount: number){
+        const newBasket = basket.slice();
+        newBasket[index] += amount
+        setBasket(newBasket);
+        console.log('cartItem was clicked')
     }
-];
+    const calculateLocalTotalPrice = (index: number) => {
+        return basket[index] * products[index].price;
+    };
+    const calculateTotalPrice = () => {
+        let totalPrice = 0;
+        for (let i = 0; i < basket.length; i++) {
+            totalPrice += calculateLocalTotalPrice(i);
+        }
+        return totalPrice;
+    };
+
+    const totalQuantity = basket.reduce((total, quantity) => total + quantity, 0);
+    const totalPrice = calculateTotalPrice();
     return (
-    <>
-    </>
-  )
+        <div>
+            {products.map((product, index) => (
+                <div key={product.id} className="cartItem">
+                    <div className="description">
+                        <p>
+                            {product.name} {product.price} {product.currency}
+                        </p>
+                        <CartItem
+                            value={basket[index]}
+                            onDecrement={() => handleAmountChange(index, -1)}
+                            onIncrement={() => handleAmountChange(index, 1)}
+                        />
+                        <p>
+                            localTotalPrice: {calculateLocalTotalPrice(index)}
+                        </p>
+                    </div>
+                </div>
+            ))}
+
+            {/* Display the total quantity */}
+            <p>Total Quantity: {totalQuantity}</p>
+            <p>Total Price: {totalPrice}</p>
+        </div>
+    );
 }
+
+function CartItem({value, onIncrement, onDecrement}) {
+    return (
+        <div>
+            <button className="increase" onClick={onIncrement}>
+                +
+            </button>
+            <span>{value}</span>
+            <button className="decrease" onClick={onDecrement} disabled={value <= 0}>
+                -
+            </button>
+        </div>
+    );
+}
+
 
 export default App
