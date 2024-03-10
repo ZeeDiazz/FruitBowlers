b30ba4e35dab358591792e8f4933cec21f16ed8e
Zaid Sheikh <Zaidsh02@gmail.com>
2024-03-10 16:17:02 +0100

    Working on validation for zipcode and city

--- src/StageTwo.tsx
+++ src/StageTwo.tsx
@@ -2,41 +2,50 @@ import './StageTwo.css'
 import {useRef, useState} from "react";
 
 export default function stage2() {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
     const zipCodeRef = useRef(null);
-
+    // eslint-disable-next-line react-hooks/rules-of-hooks
     const [validZip, setValidZip] = useState(false);
-    let [diff,diffDeliveryAddress] = useState(false);
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [diff,diffDeliveryAddress] = useState(false);
     function handleSubmit(e) {
+
         e.preventDefault();
         const form = e.target;
 
         const formData = new FormData(form);
         const zipCode = formData.get('Zipcode').toString();
+        const cityName = formData.get('City').toString();
+
         const zipCodeInput = zipCodeRef.current;
 
-
-        validateZipCode(zipCode);
+        validateZipCode(zipCode, cityName);
         if(!validZip){
-            form.reset()
+            form.reset();
             //https://developer.mozilla.org/en-US/docs/Web/API/HTMLObjectElement/setCustomValidity
             //Here we found how to add custom validy messages
             zipCodeInput.setCustomValidity('Invalid Zipcode');
             zipCodeInput.reportValidity();
-
-
         }
     }
     /* Learned from lecture and https://www.valentinog.com/blog/await-react/*/
-    async function validateZipCode(zipcode:string) {
+    async function validateZipCode(zipcode:string, cityName:string) {
+
         try {
             const response = await fetch(`https://api.dataforsyningen.dk/postnumre/${zipcode}`);
-            if (!response.ok) {
+            if (!response.ok ) {
                 throw Error(response.statusText);
             }
             const { nr, navn } = await response.json();
-            console.log('Valid Zip Code');
-            console.log(`Zip Code: ${nr}, City: ${navn}`);
+            if(navn == cityName){
+                console.log('Valid Zip Code');
+                console.log(`Zip Code: ${nr}, City: ${navn}`);
+            }
+            else {
+                throw Error(response.statusText);
+            }
             setValidZip(true);
+
         } catch (error) {
             console.log('Unvalid Zip Code');
             console.log(error);
@@ -69,9 +78,10 @@ export default function stage2() {
                         <input name="Country" type="text" value="Danmark" disabled/>
                         <br/>
                         <input name="Zipcode" type="number" placeholder="ZipCode"
-                               ref={zipCodeRef}
+                               ref={zipCodeRef} required
                         />
-                        <input name="City" placeholder="City" required/>
+                        <input name="City" placeholder="City"
+                               required/>
                         <br/>
                         <input name="streetName" type="text" placeholder="Street Name" required/>
 
@@ -91,7 +101,7 @@ export default function stage2() {
     )
 }
 
-function checkboxes(diffDeliveryAddress,diff) {
+function checkboxes(diffDeliveryAddress,diff:boolean) {
     return (
         <>
             <input type="checkbox" name="Delivery Address" value="yes"  defaultChecked={true}
@@ -112,7 +122,7 @@ function checkboxes(diffDeliveryAddress,diff) {
         </>
     )
 }
-function devliveryAdress(diff) {
+function devliveryAdress(diff:boolean) {
     if(diff){
     return (
         <>
