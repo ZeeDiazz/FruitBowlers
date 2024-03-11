28bf9655641b414f29513571cf4e70dd867f5eab
FrederikVA <fredasband@gmail.com>
2024-03-11 16:45:14 +0100

    removed test
    test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import App from "./App";
-import calculateLocalTotalPrice from './App.tsx';
+import calculateLocalTotalPrice from './App.tsx';   //@TODO later
 
 describe(App.name, () => {
     it("should render", () => {
@@ -13,18 +13,3 @@ describe(App.name, () => {
     });
 });
 
-const products = [
-    { quantity: 5, discountQuantity: 3, discountPercent: 10, totalPrice: 50 },
-    // Add more product objects as needed
-];
-describe('calculateLocalTotalPrice function', () => {
-    // Test case 1: Test when quantity is greater than or equal to discountQuantity
-    it('should calculate total price with discount correctly', () => {
-        const index = 0; // Index of the product in the products array
-        const result = calculateLocalTotalPrice();
-        const expectedTotalPrice = products[index].totalPrice * products[index].quantity;
-
-        // Assert that the result matches the expected total price
-        expect(result).toBe(expectedTotalPrice);
-    });
-});
