260c1522f2aa27e675d926d3cd56339f55a991a8
Felix Schmidt <s224313@dtu.dk>
2024-04-11 12:32:36 +0200

    rendering test and mock fetching start

--- src/tests/StageBasket.test.tsx
+++ src/tests/StageBasket.test.tsx
@@ -1,18 +1,15 @@
 import {act, render, renderHook, screen} from "@testing-library/react";
-import {describe, expect, it} from "vitest";
+import { describe, expect, it} from "vitest";
 import { useFetch } from "../Components/useFetch.ts";
 import {StageBasket} from "../Stages/StageBasket.tsx";
 
-/*global.fetch = jest.fn(() =>
-    Promise.resolve({
-        response: 'ok',
-        json: () => Promise.resolve([{ id: 1, name: 'Apple'}])
-    }
-))*/
+jest.mock('../Components/useFetch.ts', () => ({
+    fetchData: jest.fn(),
+}));
 describe('StageBasket components', () => {
     it('should display loading message when products are loading', async () => {
         const { getAllByText } = render(<StageBasket />);
-        expect(getAllByText('Loading...').length).toBeGreaterThan(0);
+        expect(getAllByText('Loading...').length).toBeGreaterThan(3);
     });
     it('should display error fetching product when error occurs', async () => {
 
@@ -23,13 +20,19 @@ describe('StageBasket components', () => {
 
         const {result} = renderHook(() => useFetch(testUrl));
         expect(result.current.length).toBe(4);
-        const [, ,loading, error] = result.current
+        const [data, ,loading, error] = result.current
         expect(loading).toBe(true);
         expect(error).toBe(false);
+        // when data is fetched the loading should be false
+        await act(async () => {
+            expect(loading).toBe(true);
+        });
+        expect(data).toBeDefined();
     });
-    it('should display products', async () => {
-        await act(async () => render(<StageBasket />));
-        expect(screen.getByText('Apple')).toBeInTheDocument();
+    it('should render the products with the fetched data', async () => {
+        const { getAllByText } = render(<StageBasket />);
+        await screen.findByText('Apples');
+        expect(getAllByText('Apples').length).toBeGreaterThan(0);
     });
 
     it('should increment quantity when plus is clicked', async () => {
